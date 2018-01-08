116a19e29c730c579b75b2a4fcba6b5f5b665f7d
Daniel Tatrai <daniel.tatrai@vertis.com>
2018-01-08 18:07:57 +0100

    fixes

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import post_list, post_id, post_new_comment
+from .views import post_list
 from . import views
 
 
@@ -7,6 +7,10 @@ urlpatterns = [
     path('', post_list, name='index'),
     # ex: /polls/5/
     path('<int:post_id>/', views.post_id, name='detail'),
-    path('<int:post_id>/post_new_comment/', views.post_new_comment, name='postnewcomment'),
+    path(
+        '<int:post_id>/post_new_comment/',
+        views.post_new_comment,
+        name='postnewcomment'
+        ),
 
 ]
